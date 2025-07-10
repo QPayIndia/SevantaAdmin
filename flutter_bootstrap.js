6a83d3216fb2186f24a2efdf29b20aafed935b5b
dhineshneo <dhinesjkumar@neophrontech.com>
2025-07-10 18:30:28 +0530

    Text Changes Commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2487679900"
+    serviceWorkerVersion: "119115728"
   }
 });
