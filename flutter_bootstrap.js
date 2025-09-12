4c968ee01250567bd30fe2bb78e96295cff99d17
dhineshneo <dhinesjkumar@neophrontech.com>
2025-09-12 10:32:47 +0530

    Payout commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1095753816"
+    serviceWorkerVersion: "3545130597"
   }
 });
